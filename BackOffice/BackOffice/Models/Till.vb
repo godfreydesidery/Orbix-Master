cc835cd4b1df61b9c9b8d17da58c7b592eea166b
godfreydesidery <desideryg@gmail.com>
2021-07-27 21:08:51 +0300

    Regular

---
+++ BackOffice/BackOffice/Models/Till.vb
@@ -0,0 +1,7 @@
+﻿Public Class Till
+    Public Property id As String
+    Public Property no As String
+    Public Property name As String
+    Public Property computerName As String
+    Public Property active As Integer
+End Class
