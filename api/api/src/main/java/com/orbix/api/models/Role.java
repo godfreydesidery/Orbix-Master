05cfed7bfc11065ca83deac3cb22ab2b200c2fb8
godfreydesidery <desideryg@gmail.com>
2021-07-29 18:20:26 +0300

    Regular

---
+++ api/api/src/main/java/com/orbix/api/models/Role.java
@@ -0,0 +1,46 @@
+package com.orbix.api.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Table(name = "roles")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	@NotBlank
+	@Column(unique = true)
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	private String status = "ACTIVE";
+}
